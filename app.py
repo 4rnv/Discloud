df347a6904f09b80f5fab6344404b65bc1ecd227
4rnv <arnavsdandekar@gmail.com>
2024-12-28 21:18:52 +0530

    fsdfsd

---
+++ app.py
@@ -0,0 +1,66 @@
+import      os
+import      time
+import      asyncio
+import      discord
+from        log             import      load_logged_files, log_file_info, save_data_to_json, File
+from        dotenv          import      load_dotenv
+from        discord.ext     import      commands
+from        os              import      listdir
+from        os.path         import      isfile, join
+
+load_dotenv()
+TOKEN = os.environ['BOT_TOKEN']
+CHANNEL_ID = int(os.environ['CHANNEL_ID'])  # Add your channel ID in .env (NOT SERVER ID)
+
+intents = discord.Intents.default()
+intents.messages = True
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+logged_files = load_logged_files()
+existing_file_hashes = {file_info['hash'] for file_info in logged_files}
+
+async def upload_file_to_channel(file_path):
+    print(f"Detected new file: {file_path}")
+    channel = bot.get_channel(CHANNEL_ID)
+    if channel:
+        try:
+            with open(file_path, 'rb') as file:
+                # bot.loop.create_task(channel.send(file=discord.File(file, filename=os.path.basename(file_path))))
+                await channel.send(file=discord.File(file, filename=os.path.basename(file_path)))
+                print(f'File {file_path} uploaded successfully')
+                return True
+        except Exception as e:
+            print(f"Error uploading file {file_path}: {e}")
+            return False
+
+@bot.event
+async def on_ready():
+    running = True
+    print(f'Logged in as {bot.user}')
+    print("Bot is running, press Ctrl+C to quit")
+
+    folder_path = r'upload' # Path to folder
+
+    try:
+        while running:
+            files = [f'{folder_path}/{f}' for f in listdir(folder_path) if isfile(join(folder_path, f))]
+            for file in files:
+                file_hash = File.get_file_hash(file)
+                if file_hash not in existing_file_hashes:
+                    status = await upload_file_to_channel(file)
+                    if status:
+                        logged_files.append({
+                            'name': os.path.basename(file),
+                            'hash': file_hash,
+                            'size': File.calculate_file_size(file)
+                        })
+                        existing_file_hashes.add(file_hash)
+                        save_data_to_json('db.json', logged_files)
+            await asyncio.sleep(5)
+    except KeyboardInterrupt:
+        print("Exiting...")
+        running=False
+
+# Run the bot
+if TOKEN and CHANNEL_ID:
+    bot.run(TOKEN)
---
+++ log.py
@@ -0,0 +1,59 @@
+import      json
+import      hashlib
+from        os          import      listdir
+from        os.path     import      isfile, join, getsize
+
+class File:
+    def __init__(self,name,hash,size):
+        self.name = name
+        self.hash = hash
+        self.size = size
+
+    def calculate_file_size(file_path):
+        return getsize(file_path)
+
+    def get_file_hash(file_path, algorithm='sha256'):
+        hash_func = hashlib.new(algorithm)
+        with open(file_path, 'rb') as file:
+            while chunk := file.read(8192):
+                hash_func.update(chunk)
+        return hash_func.hexdigest()
+    
+def load_logged_files() -> list:
+    try:
+        with open('db.json', 'r') as db:
+            return json.load(db)
+    except FileNotFoundError:
+        return []
+
+def save_data_to_json(db_file, data):
+    with open(db_file, 'w') as f:
+        json.dump(data, f, indent=4)
+
+folder_path = r'upload'
+files = [f'{folder_path}/{f}' for f in listdir(folder_path) if isfile(join(folder_path, f))]
+logged_files = load_logged_files()
+existing_file_hashes = {file_info['hash'] for file_info in logged_files}
+
+def log_file_info(files, existing_file_hashes):
+    print(files)
+    print(existing_file_hashes)
+
+    for file in files:
+        file_hash = File.get_file_hash(file)
+        file_size = File.calculate_file_size(file)
+        file_obj = File(file, file_hash, file_size)
+
+        file_data = {
+            'name': file_obj.name,
+            'hash': file_obj.hash,
+            'size': file_obj.size
+        }
+
+        if file_obj.hash not in existing_file_hashes:
+            logged_files.append(file_data)
+            existing_file_hashes.add(file_obj.hash)
+
+    save_data_to_json('db.json', logged_files)
+    print(logged_files)
+    return logged_files
